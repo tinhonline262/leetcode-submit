14320505ed5cef2740538fb0f646d7a80a4ebcfe
Huynh Trong Tinh <63954351+tinhonline262@users.noreply.github.com>
2024-11-24 13:10:41 +0700

    Time: 19 ms (93.19%) | Memory: 60.9 MB (97.49%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        bool result = false;
-        unordered_set<int> digits;
-        for (int x: nums)
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; ++i)
         {
-            if (digits.find(x) != digits.end())
-            {
-                result = true;
-            }
-            digits.insert(x);
+            if (nums[i] == nums[i + 1]) return true;
         }
-        return result;
+        return false;
     }
 };
