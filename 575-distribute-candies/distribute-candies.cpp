9ab8a9849c489dfb7977a92d894304437fc69ee4
Huynh Trong Tinh <63954351+tinhonline262@users.noreply.github.com>
2024-12-02 23:14:42 +0700

    Time: 99 ms (68.57%) | Memory: 120.2 MB (48.81%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        set<int> se(candyType.begin(), candyType.end());
+        unordered_set<int> se(candyType.begin(), candyType.end());
         return min(se.size(), candyType.size() / 2);
     }
 };
